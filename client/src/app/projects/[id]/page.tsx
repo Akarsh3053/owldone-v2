329cddf55ceed20a2cfa987a10c5aa7c742aa169
Akarsh Bajpai <onlyakarsh@gmail.com>
2025-05-30 11:43:35 +0530

    complete project interface integrates

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { useState } from 'react'
-import ProjectHeader from '../ProjectHeader';
-import BoardView from '../BoardView';
+import ProjectHeader from '../(components)/ProjectHeader';
+import BoardView from '../(components)/BoardView';
+import ListView from '../(components)/ListView';
+import TimelineView from '../(components)/TimelineView';
+import TableView from '../(components)/TableView';
 
 type Props = {
     params: { id: string };
@@ -18,6 +21,15 @@ const Projects = ({ params }: Props) => {
             <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
             {activeTab === "Board" && (
                 <BoardView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+            )}
+            {activeTab === "List" && (
+                <ListView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+            )}
+            {activeTab === "Timeline" && (
+                <TimelineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+            )}
+            {activeTab === "Table" && (
+                <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
             )}
         </div>
     )
