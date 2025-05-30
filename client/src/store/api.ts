d17a83d81ba6bffbbab415b9d32e6db6cf83883d
Akarsh Bajpai <onlyakarsh@gmail.com>
2025-05-30 16:38:07 +0530

    menu section completed v2

--- client/src/store/api.ts
+++ client/src/store/api.ts
@@ -60,6 +60,12 @@ export interface Task {
   attachments?: Attachment[];
 }
 
+export interface SearchResults {
+  tasks?: Task[];
+  projects?: Project[];
+  users?: User[];
+}
+
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
@@ -110,7 +116,18 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
+    search: build.query<SearchResults, string>({
+      query: (query) => `search?query=${query}`,
+    }),
   }),
 });
 
-export const { useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation, useUpdateTaskStatusMutation, useGetTasksByUserQuery} = api;
+export const { 
+  useGetProjectsQuery, 
+  useCreateProjectMutation, 
+  useGetTasksQuery, 
+  useCreateTaskMutation, 
+  useUpdateTaskStatusMutation, 
+  useGetTasksByUserQuery,
+  useSearchQuery
+} = api;
